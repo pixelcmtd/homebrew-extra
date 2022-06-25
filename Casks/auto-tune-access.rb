4b58ccecee04e3ff84fbca7d7e692df16dc01a78
pixel <chrissx@chrissx.de>
2022-06-25 12:25:21 +0200

    auto tune access looks more promising...maybe

---
+++ Casks/auto-tune-access.rb
@@ -0,0 +1,10 @@
+cask "auto-tune-access" do
+  version "9.1.0"
+  sha256 "dd4010108dc4258536e73f13a088c200c7f7b159d50a97dbf4bcb09b81f5eaca"
+
+  url "https://autotune.sfo2.digitaloceanspaces.com/Auto-Tune_Access/Auto-Tune_Access_v9.1.0_macOS.zip"
+  name "Auto-Tune Access"
+  homepage "https://www.antarestech.com/product/auto-tune-access/"
+
+  pkg "Auto-Tune Access v9.1.0.pkg"
+end
