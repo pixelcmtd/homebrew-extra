d5bd40a578b2b9b27b8ee86840cf8d06552d5d79
pixel <chrissx@chrissx.de>
2022-06-25 12:38:37 +0200

    dfx geometer cask (should be a formula but who can do it properly)

---
+++ Casks/dfx-geometer.rb
@@ -0,0 +1,10 @@
+cask "dfx-geometer" do
+  version "1.2.1"
+  sha256 "6bb3857d645c8dc55f18ed71cdc1cef810bfde7ac58370b96353f9eb0ffeca62"
+
+  url "http://destroyfx.org/software/geometer-1.2.1-mac.dmg"
+  name "destroyfx Geometer"
+  homepage "http://destroyfx.org"
+
+  pkg "Install Geometer.pkg"
+end
