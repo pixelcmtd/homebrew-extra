8292f2b1309b4a0d92bd9a14e29cffb13ede0a2e
pixel <chrissx@chrissx.de>
2022-06-25 12:10:37 +0200

    lets try xln online installer
    will probably break at least

---
+++ Casks/xln.rb
@@ -0,0 +1,11 @@
+cask "xln" do
+  version "96"
+  sha256 "43c757c2bf41e934ea87950a9fad62a72164339232f0493790cd0e57301abc63"
+
+  url "https://www.xlnaudio.com/api/download/96"
+  name "XLN Online Installer"
+  desc "The…thing for managing your XLN products"
+  homepage "https://www.xlnaudio.com/"
+
+  app "XLN Online Installer.app"
+end
