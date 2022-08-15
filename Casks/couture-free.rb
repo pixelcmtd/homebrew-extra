191dbd9c64c577932bb0b8e03527af5705642030
pixel <pixel@chrissx.de>
2022-08-15 16:54:41 +0200

    make couture more general

--- Casks/couture-free.rb
+++ Casks/couture-free.rb
@@ -2,10 +2,10 @@ cask "couture-free" do
   version "1.6"
   sha256 "5cebcd1358f450a0b7c7003444c3febbc8764be8d401ead2a3e07cab232aa14e"
 
-  url "https://www.auburnsounds.com/downloads/Couture-FREE-1.6.zip"
-  name "Couture Free"
+  url "https://www.auburnsounds.com/downloads/Couture-FREE-#{version}.zip"
+  name "Auburn Sounds Couture Free"
   desc "A free transient processor"
   homepage "https://www.auburnsounds.com/products/Couture.html"
 
-  pkg "Couture-FREE-1.6/macOS/Couture-FREE-1.6.0.pkg"
+  pkg "Couture-FREE-#{version}/macOS/Couture-FREE-1.6.0.pkg"
 end
