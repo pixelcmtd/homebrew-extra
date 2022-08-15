7d36c4677242b44b8a3345355d8d19cb3202208d
pixel <pixel@chrissx.de>
2022-08-15 16:50:19 +0200

    add couture free

---
+++ Casks/couture-free.rb
@@ -0,0 +1,11 @@
+cask "couture-free" do
+  version "1.6"
+  sha256 "5cebcd1358f450a0b7c7003444c3febbc8764be8d401ead2a3e07cab232aa14e"
+
+  url "https://www.auburnsounds.com/downloads/Couture-FREE-1.6.zip"
+  name "Couture Free"
+  desc "A free transient processor"
+  homepage "https://www.auburnsounds.com/products/Couture.html"
+
+  pkg "macOS/Couture-FREE-1.6.0.pkg"
+end
